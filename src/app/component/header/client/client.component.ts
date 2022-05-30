8ffad53f032165adfcfd3521df8bdb0008572edc
Pham Anh Hung <hero.ulis.50@gmail.com>
2022-05-30 09:40:03 +0700

    update

---
+++ src/app/component/header/client/client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-client',
+  templateUrl: './client.component.html',
+  styleUrls: ['./client.component.css']
+})
+export class ClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
